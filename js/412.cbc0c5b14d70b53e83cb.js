84e40fcd126026f08cd2f396b7b374b05cc524a9
GitHub <actions@github.com>
2023-08-09 11:21:15 +0000

    build: remove sample page and unused packages

---
+++ js/412.cbc0c5b14d70b53e83cb.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkprovisioning_frontend=self.webpackChunkprovisioning_frontend||[]).push([[412],{48187:(n,e,t)=>{t.d(e,{Z:()=>o});var r=t(93264),u=t.n(r);const o=function(){return u().createElement(r.Fragment,null)}},28412:(n,e,t)=>{t.r(e),t.d(e,{default:()=>c});var r=t(93264),u=t.n(r),o=t(48187);const c=function(){return u().createElement(o.Z,null)}}}]);
+//# sourceMappingURL=../sourcemaps/412.cfd2d91071ee6897f0b842b1f81bacd1.js.map
