06c107934075e92d2bee4af9929ee23bfcf1b7ea
GitHub <actions@github.com>
2023-08-29 14:01:39 +0000

    feat(HMS-2444): migrate to react-query v4

---
+++ js/883.32f0aeaf547dab828717.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkprovisioning=self.webpackChunkprovisioning||[]).push([[883],{3883:(e,n,t)=>{t.r(n);var r=t(3264),u=t.n(r),a=t(3644),i=t.n(a);const l=function(){return u().createElement(r.Fragment,null)},o=function(){return u().createElement(l,null)};var c=document.getElementById("root");i().render(u().createElement(o,null),c,(function(){return c.setAttribute("data-ouia-safe",!0)}))}}]);
+//# sourceMappingURL=../sourcemaps/883.c15fc8506695758f09ead375bf36037b.js.map
