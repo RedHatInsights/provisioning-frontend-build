ed69d75612d61e986a06c4ff348a01698a025548
GitHub <actions@github.com>
2023-08-30 15:03:42 +0000

    fix(HMS-2428): reduce the bundle size

--- js/883.a82a671cec98876cb390.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkprovisioning=self.webpackChunkprovisioning||[]).push([[883],{3883:(e,n,t)=>{t.r(n);var r=t(3264),u=t.n(r),a=t(3644),i=t.n(a);const l=function(){return u().createElement(r.Fragment,null)},o=function(){return u().createElement(l,null)};var c=document.getElementById("root");i().render(u().createElement(o,null),c,(function(){return c.setAttribute("data-ouia-safe",!0)}))}}]);
-//# sourceMappingURL=../sourcemaps/883.80351819baa5493f1c8d54ff901a3e31.js.map
--- js/exposed-./RootApp.a82a671cec98876cb390.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkprovisioning=self.webpackChunkprovisioning||[]).push([[178],{8187:(n,e,t)=>{t.d(e,{Z:()=>c});var r=t(3264),u=t.n(r);const c=function(){return u().createElement(r.Fragment,null)}},8412:(n,e,t)=>{t.r(e),t.d(e,{default:()=>i});var r=t(3264),u=t.n(r),c=t(8187);const i=function(){return u().createElement(c.Z,null)}}}]);
-//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.663c242d5ff7caa6a726661dd91ceb71.js.map
